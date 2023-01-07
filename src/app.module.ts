abf89a0108bdc1be94bab77a317f2ca0f96b26ae
Ruslan Bekshenev <rbekshenev23@gmail.com>
2023-01-07 20:10:56 +0300

    add resources CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
+import { UsersModule } from './users/users.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { ListsModule } from './lists/lists.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    UsersModule,
+    WishlistsModule,
+    ListsModule,
+  ],
   controllers: [],
   providers: [],
 })
